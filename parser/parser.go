5707f8c0126f0b53883954c2fadf15df0d89a870
EmanuelFar <manofares123@gmail.com>
2024-08-27 17:49:43 +0300

    infix expression & operator precedence tests

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,17 @@ import (
   "strconv"
 )
 
+var precendences = map[token.TokenType]int {
+  token.EQ:       EQUALS,
+  token.NOT_EQ:   EQUALS,
+  token.LT:       LESSGREATER,
+  token.GT:       LESSGREATER,
+  token.PLUS:     SUM,
+  token.MINUS:    SUM,
+  token.SLASH:    PRODUCT,
+  token.ASTERISK: PRODUCT,
+}
+
 const (
   _ int = iota
   LOWEST
@@ -49,6 +60,16 @@ func New(l* lexer.Lexer) *Parser {
   p.registerPrefix(token.INT, p.parseIntegerLiteral)
   p.registerPrefix(token.BANG, p.parsePrefixExpression)
   p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+
+  p.infixParseFns = make(map[token.TokenType]infixParseFn)
+  p.registerInfix(token.PLUS, p.parseInfixExpression)
+  p.registerInfix(token.MINUS, p.parseInfixExpression)
+  p.registerInfix(token.SLASH, p.parseInfixExpression)
+  p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+  p.registerInfix(token.EQ, p.parseInfixExpression)
+  p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
+  p.registerInfix(token.LT, p.parseInfixExpression)
+  p.registerInfix(token.GT, p.parseInfixExpression)
 
   // Read two tokens to set curToken and peekToken.
   p.nextToken()
@@ -98,7 +119,7 @@ func (p* Parser) parseLetStatement() *ast.LetStatement {
   if !p.expectPeek(token.ASSIGN) {
     return nil
   }
-  // TODO: We're skipping the expressions until we
+  //We're skipping the expressions until we
   // encounter a semicolon
   for !p.curTokenIs(token.SEMICOLON) {
     p.nextToken()
@@ -112,7 +133,7 @@ func (p* Parser) parseReturnStatement() *ast.ReturnStatement {
 
   p.nextToken()
 
-  // TODO: We're skipping the expressions until we
+  //We're skipping the expressions until we
   // encounter a semicolon
   if !p.curTokenIs(token.SEMICOLON) {
     p.nextToken()
@@ -120,7 +141,7 @@ func (p* Parser) parseReturnStatement() *ast.ReturnStatement {
  return stmt
 }
 
-// this method that kicks off expression parsing 
+// this method that kicks off expression parsing
 
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
   stmt := &ast.ExpressionStatement{Token: p.curToken}
@@ -131,6 +152,30 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
   }
   return stmt
 }
+
+func (p *Parser) parseExpression(precendence int) ast.Expression {
+  // prefix is a function that parses the given Token.
+  var prefix prefixParseFn = p.prefixParseFns[p.curToken.Type]
+  if prefix == nil {
+    p.noPrefixParseFnError(p.curToken.Type)
+    return nil
+  }
+  var leftExp ast.Expression = prefix()
+  
+  for !p.peekTokenIs(token.SEMICOLON) && precendence < p.peekPrecedence() {
+    var infix infixParseFn = p.infixParseFns[p.peekToken.Type]
+    if infix == nil {        // no infix expression found
+      return leftExp
+    }
+
+    p.nextToken()
+
+    leftExp = infix(leftExp) // leftExp holds left expression of the infix expression
+  }
+
+  return leftExp
+}
+
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
   expression := &ast.PrefixExpression{
@@ -146,17 +191,19 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
   return expression
 }
 
-func (p *Parser) parseExpression(precendence int) ast.Expression {
-  // prefix is a function that parses the given Token.
-  prefix := p.prefixParseFns[p.curToken.Type]
-  if prefix == nil {
-    p.noPrefixParseFnError(p.curToken.Type)
-    return nil
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+  expression := &ast.InfixExpression{
+    Token: p.curToken,
+    Operator: p.curToken.Literal,
+    Left: left,
   }
-  leftExp := prefix()
-  return leftExp
-}
+  // before curPrecedence is called, curToken is the operator.
+  precedence := p.curPrecedence()
+  p.nextToken()
+  expression.Right = p.parseExpression(precedence)
 
+  return expression
+}
 /***** Tokens parsing *****/
 
 func (p *Parser) parseIdentifier() ast.Expression {
@@ -199,6 +246,23 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
     return false
   }
 }
+
+/***** precendence check ******/
+
+func (p *Parser) peekPrecedence() int {
+  if p, ok := precendences[p.peekToken.Type]; ok {
+    return p
+  }
+  return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+  if p, ok := precendences[p.curToken.Type]; ok {
+    return p
+  }
+  return LOWEST
+}
+
 
 /***** Maps management *****/
 
